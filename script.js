3ae747ef66d10d3b135240842bee5834ddfeea72
Andy-Gauss <84386276+Andy-Gauss@users.noreply.github.com>
2026-08-06 20:42:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function goBack() {
     } else if (currentPage === 'page61') {
         showPage('page60');
     } else if (currentPage === 'page2') {
-        showPage('page72');
+        showPage('page73');
     } else if (currentPage === 'page62') {
         showPage('page61');
     } else if (currentPage === 'page63') {
@@ -161,6 +161,8 @@ function goBack() {
         showPage('page70');
     } else if (currentPage === 'page72') {
         showPage('page71');
+    } else if (currentPage === 'page73') {
+        showPage('page72');
     }
 }
 
